1117c774048d32590a40a6ac0604ad541f0c6cbe
cryptoGuru525 <jonathan@0xlancerlab.com>
2024-02-20 07:54:28 -0800

    Fixed label issue in admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,7 +4,7 @@ import SetTokenOwner from 'components/SetTokenOwner';
 export default function Admin() {
   return (
     <div className='flex items-center flex-col p-1 w-[100vw]'>
-      <h1 className='text-center text-3xl'>Airdrop</h1>
+      <h1 className='text-center text-3xl'>Admin</h1>
       <div className='flex flex-col w-full max-w-lg items-center'>
         <br />
         <SetTokenOwner />
