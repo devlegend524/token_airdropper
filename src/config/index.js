c7b9ca055050a91d04a4167c07fcf9fb77448794
cryptoGuru525 <jonathan@0xlancerlab.com>
2024-02-20 10:12:43 -0800

    updated mainnet token

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ export const AirDropperAddress = {
   5: '0x17e99256d1fBAd63ba180f7a625fB18121e77057',
 };
 export const TokenAddress = {
-  1: '0x69BE2c21Ea48D1a9C18c71D3E606c60152Ed58e3',
+  1: '0xc64FCE609cbC4e857cebdfFdE59d74f70D72B148',
   5: '0xAc998f3417758447da8Bea801098BD18098a48cA',
 };
 export const CHAIN_ID = 5;
